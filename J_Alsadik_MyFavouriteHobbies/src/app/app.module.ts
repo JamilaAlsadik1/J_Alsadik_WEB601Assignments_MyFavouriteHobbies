38f8b10a1231157a1419891accfcb676adcf4ca6
JamilaAlsadik1 <w0759725@myscc.ca>
2022-01-29 02:00:06 -0500

    delete

--- J_Alsadik_MyFavouriteHobbies/src/app/app.module.ts
+++ J_Alsadik_MyFavouriteHobbies/src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContentCardComponent } from './content-card/content-card.component';
 
@@ -12,7 +10,7 @@ import { ContentCardComponent } from './content-card/content-card.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
