cba5e9b05199fe0b3761c1a1c093678e62fce6cc
JamilaAlsadik1 <w0759725@myscc.ca>
2022-02-09 19:29:27 -0500

    update package.json and add new pipe

--- J_Alsadik_MyFavouriteHobbies/src/app/app.module.ts
+++ J_Alsadik_MyFavouriteHobbies/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ContentCardComponent } from './content-card/content-card.component';
 import { ContentListComponent } from './content-list/content-list.component';
+import { FilterArrayPipeComponent } from './filter-array-pipe/filter-array-pipe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentCardComponent,
-    ContentListComponent
+    ContentListComponent,
+    FilterArrayPipeComponent
   ],
   imports: [
     BrowserModule,
---
+++ J_Alsadik_MyFavouriteHobbies/src/app/filter-array-pipe/filter-array-pipe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-array-pipe',
+  templateUrl: './filter-array-pipe.component.html',
+  styleUrls: ['./filter-array-pipe.component.scss']
+})
+export class FilterArrayPipeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
