41ca4e9d10254e986e3366e7524dd0f11c281736
JamilaAlsadik1 <w0759725@myscc.ca>
2022-02-19 13:50:50 -0500

    last changes

--- J_Alsadik_MyFavouriteHobbies/src/app/hover-affect.directive.ts
+++ J_Alsadik_MyFavouriteHobbies/src/app/hover-affect.directive.ts
@@ -1,18 +1,39 @@
+import { style } from '@angular/animations';
+import { NgClass } from '@angular/common';
 import { Directive,ElementRef,HostListener,Input,OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appHoverAffect]'
 })
-export class HoverAffectDirective {
-
-  constructor(private eRaf:ElementRef) { }
+export class HoverAffectDirective implements OnInit {
+  @Input() styling?:string;
+ 
+  constructor(private eRaf:ElementRef) {
+    eRaf.nativeElement.style.cursor = "pointer";
+   }
   ngOnInit():void{
 
   }
   @HostListener('mouseenter') OnMouseenter(){
-    this.eRaf.nativeElement.style.textDecoration="underline";
+   if(this.styling=="weight"){
+     this.eRaf.nativeElement.style.fontWeight="bold";
+   }
+   else if(this.styling=="line"){
+     this.eRaf.nativeElement.style.textDecoration="underline";
+   }
+   else if(this.styling=="border"){
+   
+        this.eRaf.nativeElement.style.borderStyle="dotted";
+      }
+     
   }
   @HostListener('mouseleave') OnMouseleave(){
     this.eRaf.nativeElement.style.textDecoration="none";
+    this.eRaf.nativeElement.style.fontWeight="normal";
+    // // this.eRaf.nativeElement.style.borderStyle="solid";
   }
+  
+  
+  
 }
+
