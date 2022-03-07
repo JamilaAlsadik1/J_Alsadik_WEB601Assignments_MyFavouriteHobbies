6ff1daaa146abbfd66f1f49ca0670270210b4e5b
JamilaAlsadik1 <w0759725@myscc.ca>
2022-03-06 19:00:41 -0500

    changes in filterPipe

--- J_Alsadik_MyFavouriteHobbies/src/app/filter-array-pipe.pipe.ts
+++ J_Alsadik_MyFavouriteHobbies/src/app/filter-array-pipe.pipe.ts
@@ -7,14 +7,13 @@ import { content } from './helper-files/content-interface';
 export class FilterArrayPipePipe implements PipeTransform {
 
   transform(hobbies: content[], htype?:string): content[] {
-    return hobbies.filter(function(h){
-      if(h.type==htype){
-        return h.type?.length!=0;
-        
+    return hobbies.filter(h=>{
+      if(htype){
+        return h.type==htype;
       }
-     else{
-       return h.type?.length==0;
-     }
+      else{
+        return !h.type;
+      }
     });
   }
 
